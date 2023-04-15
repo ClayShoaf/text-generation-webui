09e312a51c2a5505a034ed75742ae10d41aa9967
Clay Shoaf <clayshoaf@gmail.com>
2023-04-15 15:50:10 -0400

    whoops, forgot to write the file on the last one

--- extensions/xy_grid/script.py
+++ extensions/xy_grid/script.py
@@ -119,6 +119,48 @@ def testf():
 
 # Create the interface for the extension (this runs first)
 def ui():
+    # Track changes
+    shared.gradio['max_new_tokens'].change(lambda x: testd.update({'max_new_tokens': x}), shared.gradio['max_new_tokens'], [])
+    shared.gradio['seed'].change(lambda x: testd.update({'seed': x}), shared.gradio['seed'], [])
+    shared.gradio['temperature'].change(lambda x: testd.update({'temperature': x}), shared.gradio['temperature'], [])
+    shared.gradio['top_p'].change(lambda x: testd.update({'top_p': x}), shared.gradio['top_p'], [])
+    shared.gradio['top_k'].change(lambda x: testd.update({'top_k': x}), shared.gradio['top_k'], [])
+    shared.gradio['typical_p'].change(lambda x: testd.update({'typical_p': x}), shared.gradio['typical_p'], [])
+    shared.gradio['repetition_penalty'].change(lambda x: testd.update({'repetition_penalty': x}), shared.gradio['repetition_penalty'], [])
+    shared.gradio['encoder_repetition_penalty'].change(lambda x: testd.update({'encoder_repetition_penalty': x}), shared.gradio['encoder_repetition_penalty'], [])
+    shared.gradio['no_repeat_ngram_size'].change(lambda x: testd.update({'no_repeat_ngram_size': x}), shared.gradio['no_repeat_ngram_size'], [])
+    shared.gradio['min_length'].change(lambda x: testd.update({'min_length': x}), shared.gradio['min_length'], [])
+    shared.gradio['do_sample'].change(lambda x: testd.update({'do_sample': x}), shared.gradio['do_sample'], [])
+    shared.gradio['penalty_alpha'].change(lambda x: testd.update({'penalty_alpha': x}), shared.gradio['penalty_alpha'], [])
+    shared.gradio['num_beams'].change(lambda x: testd.update({'num_beams': x}), shared.gradio['num_beams'], [])
+    shared.gradio['length_penalty'].change(lambda x: testd.update({'length_penalty': x}), shared.gradio['length_penalty'], [])
+    shared.gradio['early_stopping'].change(lambda x: testd.update({'early_stopping': x}), shared.gradio['early_stopping'], [])
+    shared.gradio['add_bos_token'].change(lambda x: testd.update({'add_bos_token': x}), shared.gradio['add_bos_token'], [])
+    shared.gradio['ban_eos_token'].change(lambda x: testd.update({'ban_eos_token': x}), shared.gradio['ban_eos_token'], [])
+    shared.gradio['truncation_length'].change(lambda x: testd.update({'truncation_length': x}), shared.gradio['truncation_length'], [])
+    shared.gradio['custom_stopping_strings'].change(lambda x: testd.update({'custom_stopping_strings': x}), shared.gradio['custom_stopping_strings'], [])
+    shared.gradio['name1'].change(lambda x: testd.update({'name1': x}), shared.gradio['name1'], [])
+    shared.gradio['name2'].change(lambda x: testd.update({'name2': x}), shared.gradio['name2'], [])
+    shared.gradio['greeting'].change(lambda x: testd.update({'greeting': x}), shared.gradio['greeting'], [])
+    shared.gradio['context'].change(lambda x: testd.update({'context': x}), shared.gradio['context'], [])
+    shared.gradio['end_of_turn'].change(lambda x: testd.update({'end_of_turn': x}), shared.gradio['end_of_turn'], [])
+    shared.gradio['chat_prompt_size'].change(lambda x: testd.update({'chat_prompt_size': x}), shared.gradio['chat_prompt_size'], [])
+    shared.gradio['chat_generation_attempts'].change(lambda x: testd.update({'chat_generation_attempts': x}), shared.gradio['chat_generation_attempts'], [])
+    shared.gradio['stop_at_newline'].change(lambda x: testd.update({'stop_at_newline': x}), shared.gradio['stop_at_newline'], [])
+    shared.gradio['mode'].change(lambda x: testd.update({'mode': x}), shared.gradio['mode'], [])
+    shared.gradio['instruction_template'].change(lambda x: testd.update({'instruction_template': x}), shared.gradio['instruction_template'], [])
+    shared.gradio['cpu_memory'].change(lambda x: testd.update({'cpu_memory': x}), shared.gradio['cpu_memory'], [])
+    shared.gradio['auto_devices'].change(lambda x: testd.update({'auto_devices': x}), shared.gradio['auto_devices'], [])
+    shared.gradio['disk'].change(lambda x: testd.update({'disk': x}), shared.gradio['disk'], [])
+    shared.gradio['cpu'].change(lambda x: testd.update({'cpu': x}), shared.gradio['cpu'], [])
+    shared.gradio['bf16'].change(lambda x: testd.update({'bf16': x}), shared.gradio['bf16'], [])
+    shared.gradio['load_in_8bit'].change(lambda x: testd.update({'load_in_8bit': x}), shared.gradio['load_in_8bit'], [])
+    shared.gradio['wbits'].change(lambda x: testd.update({'wbits': x}), shared.gradio['wbits'], [])
+    shared.gradio['groupsize'].change(lambda x: testd.update({'groupsize': x}), shared.gradio['groupsize'], [])
+    shared.gradio['model_type'].change(lambda x: testd.update({'model_type': x}), shared.gradio['model_type'], [])
+    shared.gradio['pre_layer'].change(lambda x: testd.update({'pre_layer': x}), shared.gradio['pre_layer'], [])
+    shared.gradio['gpu_memory_0'].change(lambda x: testd.update({'gpu_memory_0': x}), shared.gradio['gpu_memory_0'], [])
+
     with gr.Accordion("XY Grid", open=True):
 
         global testd
@@ -131,8 +173,6 @@ def ui():
             testd[k] = shared.gradio[k].value
             shared.gradio[k].change(lambda x: testd.update({k: x}), shared.gradio[k], [])
             print(k)
-        shared.gradio['name2'].change(lambda x: testd.update({'name2': x}), shared.gradio['name2'], [])
-        shared.gradio['name2'].change(lambda x: testd.update({'name2': x}), shared.gradio['name2'], [])
 
         prompt = gr.Textbox(placeholder="Comma separated prompts go here...", label='Input Prompts', interactive=True)
         with gr.Row():
